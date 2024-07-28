f4ee269d72db4c0eeedcd1920a94be4cf2b1215f
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-07-28 21:56:13 +0200

    Setup default admin user at startup

---
+++ src/main/java/com/accesscontrol/config/DataInitializer.java
@@ -0,0 +1,30 @@
+package com.accesscontrol.config;
+
+import com.accesscontrol.entities.Role;
+import com.accesscontrol.entities.User;
+import com.accesscontrol.services.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DataInitializer {
+
+    private static final String DEFAULT_USER = "admin";
+    private static final String DEFAULT_PASSWORD = "password";
+    private static final Role DEFAULT_ROLE = Role.ADMIN;
+
+    @Bean
+    CommandLineRunner init(UserService userService, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userService.findByUsername(DEFAULT_USER).isEmpty()) {
+                User user = new User();
+                user.setUsername(DEFAULT_USER);
+                user.setPassword(passwordEncoder, DEFAULT_PASSWORD);
+                user.setRole(DEFAULT_ROLE);
+                userService.saveUser(user);
+            }
+        };
+    }
+}
